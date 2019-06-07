7a6be62b536f2740011801687d6679987b0504db
Jefferson Silva <jgsnat@gmailcom>
2019-06-07 13:17:49 -0300

    - cors and refactoring;

--- src/config/server.js
+++ src/config/server.js
@@ -3,12 +3,14 @@ const port = 5000
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, () => 
-    console.log(`BACKEND is runnig on port ${port}`)
+    console.log(`BACKEND is runnig on port ${port}.`)
 );
 
 module.exports = server;
